99e9e1f27f1eec0478e7016a930c6d268da821e8
nwayt001 <nick.waytowich@gmail.com>
2015-10-22 18:35:37 -0400

    Updated bci_Process.m to handle to-sml
    to-sml = threshold optimized sml learning

--- Online Experiment/BCI2000/prog/matlab_RSVP/bci_Process.m
+++ Online Experiment/BCI2000/prog/matlab_RSVP/bci_Process.m
@@ -1,9 +1,9 @@
 function out_signal = bci_Process( in_signal )
-
+%%
 % Global VARS
 global bci_States modelRsvp pred_label gt_labels smlThreshold predLabel;
-global previousSignal currentSignal udpSocket;
-global ensembleLabel currentImage previousImage score;
+global previousSignal currentSignal udpSocket smlWindow;
+global ensembleLabel currentImage previousImage score thresholdArray pred_score;
 
 % do a two-block buffer of signal and image state
 out_signal=in_signal;
@@ -25,9 +25,8 @@ currentImage = bci_States.StimulusCode;
 % coresponding to a half second ago concatonated with the current block
 if(previousImage~=0) % classify
     testObs = [previousSignal currentSignal]; % concatonate previous and current blocks
-     
-%     [~, ensembleLabel] = ensembleClassify(testObs,modelRsvp);
-
+    
+    % data boosting
     testObs = CreateArtificialTrials(testObs,256,0,1,31.25);
     for j=1:size(testObs,3)
         [tmpScore(:,j), tmpLabel(:,j)] = ensembleClassify(testObs(:,:,j),...
@@ -37,28 +36,37 @@ if(previousImage~=0) % classify
     for k=1:length(idx)
         ensembleScore(k,1) = tmpScore(k,idx(k));
     end
-    ensembleLabel = sign(ensembleScore);
     
+    pred_score = [pred_score ensembleScore];
+    ensembleLabel = sign(ensembleScore);
     pred_label = [pred_label ensembleLabel];
     
+    if(size(pred_label,2)>smlWindow)
+        thresh_labels=zeros(length(thresholdArray),smlWindow);
+    else
+        thresh_labels=zeros(length(thresholdArray),size(pred_label,2));
+    end
     if(size(pred_label,2) > smlThreshold) %apply sml if enough observations
-         if(size(pred_label,2)>200)
-             smlWeight = applySML(pred_label(:,end-199:end));
-         else
-            smlWeight = applySML(pred_label);
-         end
-%         score = smlWeight'* pred_label; % <- label refining
-        score = smlWeight'* ensembleLabel; % <- No label refining
+        for thresh = 1:length(thresholdArray)
+            if(size(pred_label,2)>smlWindow)
+                new_score = pred_score(:,end-(smlWindow-1):end) - thresholdArray(thresh);
+            else
+                new_score = pred_score - thresholdArray(thresh);
+            end
+            new_label = sign(new_score);
+            smlWeight = applySML(new_label);
+            thresh_labels(thresh,:)=sign(smlWeight'*new_label);
+        end
+        smlWeight = applySML(thresh_labels);
+        score = smlWeight'*thresh_labels(:,end);
     else
         weight = (1/size(pred_label,1)) * ones( size(pred_label,1),1 );
-%         score = weight'* pred_label; % <- label refining
         score = weight'* ensembleLabel; % <- No label refining
     end
-%     predLabel = sign(score); % <- label refining
     predLabel = [predLabel sign(score)]; % <- No label refining
     
-    % UDP feedback (images 217 -240 are targets, all else are
-    % non-targets)
+    
+    %% Feedback
     % UDP code - 'Prediction(T or N):ImageNumber(1-240):S:score'
     if(sign(score)>=0)
         fb = ['T' int2str(previousImage) 'S' num2str(score)];
